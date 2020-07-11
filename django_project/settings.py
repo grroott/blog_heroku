2dfbf74faa90e6be395728212b8741b43b0fdee5
Gokul nath <gokulnath634@gmail.com>
2020-07-11 12:12:16 +0530

    Debug value set to false -

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'Tru')
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'Tru')
 
 ALLOWED_HOSTS = ['grootblog.herokuapp.com']
 
