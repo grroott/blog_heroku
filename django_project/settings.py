1db03cc4830fd6fa3e3985e7569bd0e56e0caef8
Gokul nath <gokulnath634@gmail.com>
2020-07-11 11:57:25 +0530

    Debug value from environment

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,6 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+
 ALLOWED_HOSTS = ['grootblog.herokuapp.com']
 
 
